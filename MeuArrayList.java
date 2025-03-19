6303b0caf405e1141c3d44e479f2896eaa6fd143
Rodrigo Bossini <professorbossini@gmail.com>
2025-03-19 09:08:27 -0300

    feat: implementa arraylist e linkedlist

---
+++ MeuArrayList.java
@@ -0,0 +1,24 @@
+public class MeuArrayList {
+    int [] elementos;
+    int qtde = 0, cap;
+
+    MeuArrayList(int cap){
+        this.cap = cap;
+    }
+
+    public void adicionar(int elemento){
+        if(estaCheio()){
+            var aux = new int[cap * 2];
+            for(int i = 0; i < cap; i++){
+                aux[i] = elementos[i];
+            }
+            cap *= 2;
+            elementos = aux;
+        }
+        elementos[qtde++] = elemento;
+    }
+
+    public boolean estaCheio(){
+        return qtde == cap;
+    }
+}
---
+++ MinhaListaLigada.java
@@ -0,0 +1,25 @@
+public class MinhaListaLigada {
+    No primeiro = null, ultimo = null;
+
+    public void adicionar(Integer i){
+        var no = new No(i);
+        if (primeiro == null){
+            primeiro = no;
+            ultimo = no;
+        }
+        else{
+            ultimo.proximo = no;
+            no.anterior = ultimo;
+            ultimo = no;
+        }
+    }
+}
+
+class No{
+    Integer item;
+    No anterior = null;
+    No proximo = null;
+    No (Integer item){
+        this.item = item;
+    }
+}
