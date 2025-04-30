34a71b7fb1225f4d99770e5e4ebfe8ddea1ef55f
Rodrigo Bossini <professorbossini@gmail.com>
2025-04-30 08:56:34 -0300

    feat(dijkstra): encerra o algoritmo

--- menores_caminhos_de_origem_unica/Dijkstra.java
+++ menores_caminhos_de_origem_unica/Dijkstra.java
@@ -65,7 +65,14 @@ class Grafo{
 public class Dijkstra {
 
   public void executar(Grafo g, int s){
-
+    inicializarFonteUnica(g, s);
+    List <Grafo.Vertice> q = new ArrayList<>(List.of(g.getVertices()));
+    while(!q.isEmpty()){
+      var u = extrairMinimo(q);
+      for(Grafo.Aresta aresta : g.vizinhos(u.indice)){
+        relaxar(u, g.getVertices()[aresta.destino], aresta.peso);  
+      }
+    }
   }
 
   private void relaxar(Grafo.Vertice u, Grafo.Vertice v, int w){
@@ -75,7 +82,47 @@ public class Dijkstra {
     }
   }
 
-  public static void main (String [] args){
+  private void inicializarFonteUnica (Grafo g, int s){
+    for(Grafo.Vertice v : g.getVertices()){
+      v.distancia = Integer.MAX_VALUE;
+      v.predecessor = null;  
+    }
+    g.getVertices()[s].distancia = 0;
+  }
+  
+  private Grafo.Vertice extrairMinimo (List <Grafo.Vertice> q){
+    int indiceMin = 0;
+    for(int i = 1; i < q.size(); i++){
+      if(q.get(i).distancia < q.get(indiceMin).distancia){
+        indiceMin = i;
+      }
+    }   
+    return q.remove(indiceMin);
+  }
 
+
+
+  public static void main (String [] args){
+    System.out.println("oi");
+    String [] nomes = {"s", "t", "x", "y", "z"};
+    Grafo g = new Grafo(nomes);
+    g.adicionarAresta(0, 1, 10);
+    g.adicionarAresta(0, 3, 5);
+    g.adicionarAresta(1, 2, 1);
+    g.adicionarAresta(1, 3, 2);
+    g.adicionarAresta(2, 4, 4);
+    g.adicionarAresta(3, 1, 3);
+    g.adicionarAresta(3, 2, 9);
+    g.adicionarAresta(3, 4, 2);
+    g.adicionarAresta(4, 2, 6);
+    var dijkstra = new Dijkstra();
+    dijkstra.executar(g, 0);
+    for (Grafo.Vertice v : g.getVertices()){
+      System.out.printf("Distância de s a %s: %d\n", v.nome, v.distancia);
+      System.out.printf(
+        "%s\n", v.predecessor != null ? "Predecessor de " + v.nome + ": " + v.predecessor.nome : "Não tem predecessor"
+      );
+    }
   }
 }
+
